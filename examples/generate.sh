580e4969feb9bcf23f6b619916d511e1d77eb6b7
Max Melching <mmelchin@caltech.edu>
2026-06-10 09:40:23 -0500

    Adding examples.

---
+++ examples/generate.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+
+python ../plot_timing_circles.py \
+    --skymap-file sim_id_4.fits \
+    --stats-file combined_stats.dat \
+    --injection-number 4 \
+    --plot-freq 56 \
+    --ring-pairs L1-H1 L1-V1 H1-V1 \
+    --contour-levels 50 90 \
+    --outdir . \
+    --no-show
+
+python ../plot_timing_circles.py \
+    --skymap-file sim_id_4.fits \
+    --stats-file combined_stats.dat \
+    --injection-number 4 \
+    --plot-freq 56 \
+    --ring-pairs L1-H1 L1-V1 H1-V1 \
+    --contour-levels 50 90 \
+    --geo \
+    --outdir . \
+    --no-show
+
+python ../plot_timing_circles.py \
+    --skymap-file sim_id_4.fits \
+    --stats-file combined_stats.dat \
+    --injection-number 4 \
+    --plot-freq 56 \
+    --ring-pairs L1-H1 L1-V1 H1-V1 \
+    --timing-uncertainty \
+    --timing-sigma-ms 0.25 \
+    --n-annulus 67 \
+    --contour-levels 50 90 \
+    --outdir . \
+    --no-show
+
+python ../plot_timing_circles.py \
+    --skymap-file sim_id_4.fits \
+    --stats-file combined_stats.dat \
+    --injection-number 4 \
+    --plot-freq 56 \
+    --ring-pairs L1-H1 L1-V1 H1-V1 \
+    --timing-uncertainty \
+    --timing-sigma-ms 0.25 \
+    --n-annulus 96 \
+    --contour-levels 50 90 \
+    --geo \
+    --outdir . \
+    --no-show
+
+python ../plot_timing_circles.py \
+    --skymap-file sim_id_25.fits \
+    --stats-file combined_stats.dat \
+    --injection-number 25 \
+    --plot-freq 1024 \
+    --ring-pairs L1-H1 L1-V1 H1-V1 \
+    --timing-uncertainty \
+    --timing-sigma-ms 0.25 \
+    --n-annulus 67 \
+    --contour-levels 50 90 \
+    --outdir . \
+    --no-show
+
+python ../plot_timing_circles.py \
+    --skymap-file sim_id_25.fits \
+    --stats-file combined_stats.dat \
+    --injection-number 25 \
+    --plot-freq 1024 \
+    --ring-pairs L1-H1 L1-V1 H1-V1 \
+    --timing-uncertainty \
+    --timing-sigma-ms 0.25 \
+    --n-annulus 96 \
+    --contour-levels 50 90 \
+    --geo \
+    --outdir . \
+    --no-show
+
+# -- Might have mainly been a two-detector injection
+
+
+python ../plot_timing_circles.py \
+    --bilby-json hv_true_snr_fixed_spins_nohom_zero_noise_result.json \
+    --ring-pairs L1-H1 L1-V1 H1-V1 \
+    --contour-levels 50 90 \
+    --outdir . \
+    --no-show
+
+
+python ../plot_timing_circles.py \
+    --sky-pos 97.2 -35.7 1187008882 \
+    --ring-pairs L1-H1 L1-V1 H1-V1 \
+    --timing-uncertainty --timing-sigma-ms 0.5 \
+    --geo \
+    --outdir . \
